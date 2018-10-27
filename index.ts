086f9d6c5e62ed26177f593d684973b65922c355
Chris Wheeldon <cdwheeldon@gmail.com>
2018-10-27 11:13:22 +0100

    Working txtindex£

--- index.ts
+++ index.ts
@@ -1,14 +1,13 @@
-import * as fs from 'fs';
-import * as readline from 'readline';
+import * as fs from "fs";
+import * as readline from "readline";
 
 interface index {
-  start: number,
-  end: number,
-  children: {[key:string]: index}
+  start: number;
+  end: number;
+  children: { [key: string]: index };
 }
 
 class indexer {
-
   _stack: index[];
   _prefix: string;
   _lineno: number;
@@ -19,19 +18,22 @@ class indexer {
     this._lineno = 0;
   }
 
-  public index(filename: string, keyfunc: (line: string) => string): Promise<index> {
+  public index(
+    filename: string,
+    keyfunc: (line: string) => string
+  ): Promise<index> {
     this._stack.push({ start: 0, end: 0, children: {} });
     this._prefix = "";
-    
+
     return new Promise<index>((resolve, _) => {
       const rs = fs.createReadStream(filename);
       const rl = readline.createInterface(rs);
-      rl.on('line', (line) => {
+      rl.on("line", line => {
         line = keyfunc(line);
         this.process(line);
       });
-      rl.on('close', () => {
-        resolve(this.finish() || { start: 0, end: 0, children: { } });
+      rl.on("close", () => {
+        resolve(this.finish() || { start: 0, end: 0, children: {} });
       });
     });
   }
@@ -40,10 +42,10 @@ class indexer {
     if (!line.startsWith(this._prefix)) {
       this.exit(line);
     }
-    if (line != this._prefix && this._stack.length < 10) {
+    if (line != this._prefix && this._stack.length < 8) {
       this.enter(line);
     }
-    this._lineno += 1;
+    this._lineno++;
   }
 
   private enter(line: string) {
@@ -51,14 +53,14 @@ class indexer {
     const node = {
       start: this._lineno,
       end: this._lineno,
-      children: { }
+      children: {}
     };
     this._stack[this._stack.length - 1].children[key] = node;
     this._stack.push(node);
     this._prefix = this._prefix + key;
   }
 
-  private exit(line: string)  {
+  private exit(line: string) {
     while (!line.startsWith(this._prefix)) {
       this.pop();
     }
@@ -80,17 +82,30 @@ class indexer {
 }
 
 export class txtindx {
-
   _filename: string;
   _keyfunc: (line: string) => string;
+  _index: Promise<index>;
 
   constructor(filename: string, keyfunc: (line: string) => string) {
     this._filename = filename;
     this._keyfunc = keyfunc;
+    this._index = Promise.resolve({ start: 0, end: 0, children: {} });
   }
 
   public index(): Promise<index> {
-    return new indexer().index(this._filename, this._keyfunc);
+    this._index = new indexer().index(this._filename, this._keyfunc);
+    return this._index;
   }
 
+  public async lookup(key: string): Promise<index | null> {
+    let index = await this._index;
+    for (let i = 0; i < key.length; ++i) {
+      const ki = key[i];
+      if (!index.children[ki]) {
+        break;
+      }
+      index = index.children[ki];
+    }
+    return index;
+  }
 }
--- test.ts
+++ test.ts
@@ -1,13 +1,18 @@
-import { txtindx } from './index';
+import { txtindx } from "./index";
 
 var filename = process.argv[2];
 console.log(filename);
 const ti = new txtindx(filename, (line: string) => {
-  if (line.startsWith('!_TAG_')) {
+  if (line.startsWith("!_TAG_")) {
     return "";
   }
-  return line.split('\t')[0];
+  return line.split("\t")[0];
 });
-ti.index().then((index) => {
+ti.index().then(index => {
   console.log(process.memoryUsage().heapUsed);
+  ti.lookup("usb_").then(index => {
+    if (index) {
+      console.log(`${index.start} ${index.end}`);
+    }
+  });
 });
